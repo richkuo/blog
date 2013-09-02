121372732548cebfe8cc674c854154b9737e5cd6
richkuo <richard.p.kuo@gmail.com>
2013-09-02 02:54:56 -0400

    refactoring some code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  
+  def top_categories(n)
+    @categories = []
+    Category.all.each do |c|
+      @categories << c
+    end
+    @categories.sort_by! { |c| c.posts.count }
+  end
+
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -5,16 +5,4 @@ class Category
   
   has_and_belongs_to_many :posts
 
-  private
-
-  def most_popular(n)
-    # returns the most popular n categories
-    popular = []
-    while popular.size < n
-      Category.all.each do |c|
-        popular.last && popular.last.posts.count > c.posts.count ? popular << c : popular.push(c)
-      end
-    end
-  end
-
 end
